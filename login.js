85ad3cb54e0bf48d73dd4dcb56a3b3ae2a174453
Husam <hshaik@live.unc.edu>
2018-12-09 18:01:05 -0500

    Update login.js

--- login.js
+++ login.js
@@ -38,8 +38,8 @@ $(document).ready(() => {
         body.append($('<p>USER: ' +user+ '</p>')); 
 
         //Buttons
-        var singleTripBTN = $('<button id = "singleBtn" class ="singleTripBTN">Single Trip</button>').click(()=>{});
-        var roundTripBTN = $('<button id = "roundBtn" class ="roundTripBTN">Round Trip</button>').click(()=>{buildRoundtripSearchInterface();});
+        var singleTripBTN = $('<button id = "singleBtn" class ="singleTripBTN">Single Trip</button>').click(()=>{buildSingletripSearchInterface();});
+        var roundTripBTN = $('<button id = "roundBtn" class ="roundTripBTN">Round Trip</button>').click(()=>{buildRoundtripSearchInterface()});
         var iteneraryBTN = $('<button id = "itenerary" class ="iteneraryBTN">My Flights</button>').click(()=>{});
 
         var buttoDiv = $('<div class = "buttonDiv"></div>'); 
