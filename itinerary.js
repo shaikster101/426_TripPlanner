8d49066297fc143f14d2a5d67d073767e38c353b
Husam <hshaik@live.unc.edu>
2018-12-12 15:09:19 -0500

    Update itinerary.js

--- itinerary.js
+++ itinerary.js
@@ -1,3 +1,4 @@
+tickets = []
 var buildItenerary = function(){
 
 
@@ -12,5 +13,34 @@ var buildItenerary = function(){
     var backHome = $('<button id="backHome">Go Back Home</button>').click(() =>{homePage(user);});
     body.append(backHome);
     body.append(header); 
+
+    $.ajax(root_url + `tickets?filter[user_id]=${userId}`, 
+    {   type: 'GET', 
+        xhrFields: {withCredentials: true}, 
+        data: {},
+        success: (response) => { 
+            tickets = response; 
+            console.log(tickets);
+        }
+    });
+
+
+    console.log(tickets.length); 
+
+    for(let i = 0; i < tickets.length; i++){
+        var fname = tickets[i].name; 
+        var gender = tickets[i].gender; 
+        var age = ticket[i].age;
+        console.log(age); 
+        alert("this works"); 
+
+        var tempDiv = $('<div id="ticket"></div>');
+
+        tempDiv.append(age); 
+        ticketDiv.append(tempDiv); 
+
+    }
+
+    body.append(ticketDiv); 
 
 }
