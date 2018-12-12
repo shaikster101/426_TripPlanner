4bc703a83f86a5c29ea7bdd8692fea22892863c3
Husam <hshaik@live.unc.edu>
2018-12-12 14:00:59 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 var root_url = "http://comp426.cs.unc.edu:3001/"
 let userId = undefined;
 
+let user; 
 $(document).ready(() => {
 
     $('#login_btn').on('click', () => {
@@ -26,48 +27,7 @@ $(document).ready(() => {
     });
 
 
-    var homePage = function(user){
-        var body = $('body')
-        body.empty();
-        body.removeClass("loginbody");
-        body.addClass("homepagebody");
-        var flightContainer = $('<div id="flightContainer"></div>')
-        var logoutBtn = $('<button id = "logout">Logout</button>').click(() =>{window.location.reload()});
-        body.append(logoutBtn);
-        
-        
-        
-
-        body.append($('<h1>Tar Heel Travel Planner</h1>'));
-
-        body.append($('<p>You are logged in as: ' +user+ '</p>')); 
-        body.append($('<p1>Make a selection from the panel below: </p1>'));
-       // body.append($('<img id = "cloud" src = "cloud image.png">'))
-        //Buttons
-        var singleTripBTN = $('<button id = "singleBtn" class ="singleTripBTN">Single Trip</button>').click(()=>{buildSingletripSearchInterface();});
-        //var roundTripBTN = $('<button id = "roundBtn" class ="roundTripBTN">Round Trip</button>').click(()=>{buildRoundtripSearchInterface()});
-        var iteneraryBTN = $('<button id = "itenerary" class ="iteneraryBTN">My Flights</button>').click(()=>{});
-
-        var buttoDiv = $('<div class = "buttonDiv"></div>'); 
-
-        buttoDiv.append(singleTripBTN);
-        //buttoDiv.append(roundTripBTN); 
-        buttoDiv.append(iteneraryBTN); 
-        
-        body.append(buttoDiv); 
-        body.append(flightContainer); 
-
-        $.ajax(root_url + `users?filter[username]=${user}`, 
-        {   type: 'GET', 
-            xhrFields: {withCredentials: true}, 
-            data: {},
-            success: (response) => { 
-                userId = response.id;
-            }
-        });
-       
-
-    }
+   
     
 
     $('#create_user_btn').on('click', () => {
@@ -105,7 +65,7 @@ $(document).ready(() => {
 var createUser = function(){
 
     var login_url = root_url + "users"; 
-    var user = $('#login_user').val();
+    user = $('#login_user').val();
     var pass = $('#password_user').val();
 
     alert(user); 
@@ -123,4 +83,47 @@ var createUser = function(){
                 console.log("Creation Worked"); 
             }
     });
+}
+
+var homePage = function(user){
+    var body = $('body')
+    body.empty();
+    body.removeClass("loginbody");
+    body.addClass("homepagebody");
+    var flightContainer = $('<div id="flightContainer"></div>')
+    var logoutBtn = $('<button id = "logout">Logout</button>').click(() =>{window.location.reload()});
+    body.append(logoutBtn);
+    
+    
+    
+
+    body.append($('<h1>Tar Heel Travel Planner</h1>'));
+
+    body.append($('<p>You are logged in as: ' +user+ '</p>')); 
+    body.append($('<p1>Make a selection from the panel below: </p1>'));
+   // body.append($('<img id = "cloud" src = "cloud image.png">'))
+    //Buttons
+    var singleTripBTN = $('<button id = "singleBtn" class ="singleTripBTN">Single Trip</button>').click(()=>{buildSingletripSearchInterface();});
+    //var roundTripBTN = $('<button id = "roundBtn" class ="roundTripBTN">Round Trip</button>').click(()=>{buildRoundtripSearchInterface()});
+    var iteneraryBTN = $('<button id = "itenerary" class ="iteneraryBTN">My Flights</button>').click(()=>{buildItenerary();});
+
+    var buttoDiv = $('<div class = "buttonDiv"></div>'); 
+
+    buttoDiv.append(singleTripBTN);
+    //buttoDiv.append(roundTripBTN); 
+    buttoDiv.append(iteneraryBTN); 
+    
+    body.append(buttoDiv); 
+    body.append(flightContainer); 
+
+    $.ajax(root_url + `users?filter[username]=${user}`, 
+    {   type: 'GET', 
+        xhrFields: {withCredentials: true}, 
+        data: {},
+        success: (response) => { 
+            userId = response.id;
+        }
+    });
+   
+
 }
