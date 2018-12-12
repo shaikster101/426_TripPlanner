85006034a06b8374b1c6218fbc522fbe56d3eb90
Rupesh <rupesh@live.unc.edu>
2018-12-12 13:35:53 -0500

    Update login.js

--- login.js
+++ login.js
@@ -81,13 +81,13 @@ $(document).ready(() => {
         var userDiv = $('<div class="userDiv"></div>');
         var passDiv = $('<div class="passDiv"></div>');
 
-        body.append("<h1>Create New User</h1>");
+        body.append("<h1>Make an Account</h1>");
 
-        userDiv.append("User");
+        userDiv.append("Username:");
         var userIn = $('<input type="text" id="login_user" class = "user_input"></input>');
         userDiv.append(userIn); 
 
-        passDiv.append("Password");
+        passDiv.append("Password:");
         var passIn = $('<input type="text" id="password_user" class = "password_input"></input>');
         passDiv.append(passIn); 
 
@@ -95,7 +95,7 @@ $(document).ready(() => {
         loginDiv.append(passDiv);
         body.append(loginDiv); 
         
-        body.append($('<button class= "create"> Create User</button>').click(()=>{createUser()})); 
+        body.append($('<button class= "create"> Create Account</button>').click(()=>{createUser()})); 
 
         
     });
