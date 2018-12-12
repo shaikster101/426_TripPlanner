720960f154e3ef4e519161ae8dba4b21b023c578
Husam <hshaik@live.unc.edu>
2018-12-12 11:26:20 -0500

    Update single.js

--- single.js
+++ single.js
@@ -11,6 +11,8 @@ let confirmationCode = undefined;
 let homeLat; 
 let homeLog; 
 
+var map;
+
 let Dlat; 
 let Dlog; 
 
@@ -52,9 +54,10 @@ let buildSingletripSearchInterface = function() {
     $('#flight-search-button-div').append('<input type="button" id="flight-search-button" value="Search">');
 
     $('#flight-search-button').on('click', function(){
-    	getAirportCoordinates();
-    });
-
+    	flightSearch();
+	});
+	
+	
 	retrieveAirports();
 }
 
@@ -220,6 +223,7 @@ let instanceSearch = function(flights) {
 var buildConfirmationPage =  function(departureFlightsArr){
 	
 
+	var mapDiv = $('<div id="map"></div>')
 
 	let departureCity = document.getElementById("li_"+departureAirportId).childNodes[1].innerHTML; 
 	let destinationCity = document.getElementById("li_"+destinationAirportId).childNodes[1].innerHTML;
@@ -277,12 +281,20 @@ var buildConfirmationPage =  function(departureFlightsArr){
 	body.append(confirmation); 
 	body.append('<input type="text" id="Name" ></input>' + '<br>');
 	body.append('<input type="text" id="Email"></input>'+'<br>')
+	
 
 	var confirmBtn = $('<button id=confirmBooking> Confirm Booking</button>').click(()=>{
 		finalConfirm();});
 
 	body.append(confirmBtn); 
+	body.append(mapDiv);
 
+	var script = document.createElement('script')
+	script.type = 'text/javascript';
+	script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB2h3SW6HJljk-cwQ3hutFYPepUq-XyUtE&callback=initMap';
+
+	document.body.appendChild(script)
+	createMap();
 
 
 }
@@ -366,7 +378,12 @@ var finalConfirm = function(){
 	
 	body.append(confirmHeader); 
 	body.append(backBtn); 
-
-
 }
 
+
+var createMap = function(){
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	  });
+}
