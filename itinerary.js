6d544d562ba1af8cd9f625707bb3a5266452aba3
Husam <hshaik@live.unc.edu>
2018-12-12 14:00:57 -0500

    Update itinerary.js

--- itinerary.js
+++ itinerary.js
@@ -4,14 +4,13 @@ var buildItenerary = function(){
     var body = $('body'); 
     body.empty(); 
 
-    var header = $('<h1 id= "mainItenerary"> My Tickets<>');
+    var header = $('<h1 id= "mainItenerary"> My Tickets </h1>');
 
 
     var ticketDiv = $('<div id="ticketHolder"></div>')
 
+    var backHome = $('<button id="backHome">Go Back Home</button>').click(() =>{homePage(user);});
+    body.append(backHome);
+    body.append(header); 
 
-    
-
-
-    
 }
